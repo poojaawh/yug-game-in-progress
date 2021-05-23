a1e4c919695da6176afb1189ef32e16580058ed5
poojaawh <63486166+poojaawh@users.noreply.github.com>
2021-05-23 16:58:12 +0530

    Add files via upload

---
+++ gamek/lab.js
@@ -0,0 +1,29 @@
+class Lab{
+    constructor(){
+        this.scientist = loadImage("person.png")
+       // this.base = createSprite(400,200,800,400)
+        this.people = createSprite(-200,-200,50,100)
+
+    }
+    display(){
+        this.people.x = 100;
+        this.people.y = 200;
+        
+        this.people.addImage(this.scientist)
+    // people.shapeColor = "red"
+        textSize(17)
+        stroke("green")
+        fill("orange")
+        text("OK now i have created the world's first size shrinking formula,   uhh uhh why am i coughing.", 10, 320)
+        stroke("orange")
+        text("DIES*", 20,350)
+    }
+
+    update(state){
+        gameState = state;
+    }
+    remove(){
+        this.people.destroy();
+    }
+
+}
